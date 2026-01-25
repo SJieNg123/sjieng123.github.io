2b2a557c32c0521986038847ab7c96e5c62fb6dd
sj <shijie.jh@gmail.com>
2026-01-26 01:24:57 +0800

    Update: Fix package issue

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from "./pages/Home";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/personal-website">
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
