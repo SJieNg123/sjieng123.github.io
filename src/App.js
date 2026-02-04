8e2235943bdc78fc5c433b37816ebe65e38db6ee
sjieng123 <sjieng123@users.noreply.github.com>
2026-02-05 03:04:00 +0800

    Update: Update github pages url

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from "./pages/Home";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="/personal-website">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
